3962e97f11640ae63ce4bf72bd05c427c9823fcc
FELIPE TORRES GONZALEZ <felipe.gonzalez@etec.sp.gov.br>
2025-04-03 17:42:31 -0300

    salvando meu codigo

---
+++ Calculadora/App.js
@@ -0,0 +1,25 @@
+import {StyleSheet, View} from 'react-native'
+import Title from './src/components/Title'
+import FormIMC from './src/components/FormIMC'
+import Classification from './src/components/Classification';
+
+
+
+export default function App() {
+    return(
+        <View style={styles.container}>
+            <Title />
+            <FormIMC />
+            <Classification />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 16,
+        backgroundColor: '#fff',
+    },
+});
